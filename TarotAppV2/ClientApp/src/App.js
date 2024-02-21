e0a11dc75c381081d40c77b9d083927266411a71
kristina <kristina.ancimer@gmail.com>
2024-02-21 15:42:29 +0100

    uredena TarotReading komponenta, maknut dio nepotrebnog sadrzaja

--- TarotAppV2/ClientApp/src/App.js
+++ TarotAppV2/ClientApp/src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import AppRoutes from './AppRoutes';
-import { Layout } from './components/Layout';
+import { Container } from 'reactstrap';
 import './custom.css';
-import "./fonts/CenturyGothic.ttf";
-import "./fonts/Cormorant-VariableFont_wght.ttf";
+
+import TarotReading from './components/TarotReading';
 
 
 function App() { 
    
-        return ( 
-            <Layout>
-               
-                    
-               
-                <Routes>
-                    {AppRoutes.map((route, index) => {
-                        const { element, ...rest } = route;
-                        return <Route key={index} {...rest} element={element} />;
-                    })}
-                </Routes>
-            </Layout> 
+    return (
+        <Container>
+            <TarotReading />
+        </Container>
         );
     }
 
--- TarotAppV2/ClientApp/src/components/TarotReading.js
+++ TarotAppV2/ClientApp/src/components/TarotReading.js
@@ -26,14 +26,12 @@ const TarotReading = () => {
         setTimeout(() => {
             scrollToCard();
         }, 500);
-        //console.log("clicked!");
-       
+        
     };
-    //const styleAnimation = ` ${isCardClicked ? 'display-reading' : 'dont-display'}`;
-    //const styleAnimation = `'dont-display'`;
+    
    
     let starryBackground = (<StarryBackground
-            densityRatio={0.75} // Set default values or customize as needed
+            densityRatio={0.75} 
             sizeLimit={10}
             scaleLimit={15}
             proximityRatio={0.2}
@@ -41,13 +39,9 @@ const TarotReading = () => {
             className="backgroundSetting"
         />
     );
-    /*const scrollToCard = () => {
-        let card = document.getElementById('tarot-card');
-        card.scrollIntoView();
-        scrollRef.current?.scrollIntoView({ behavior: 'smooth' });
-    }*/
+    
     const scrollToCard = () => {
-        // If the TarotCard has an ID, use it; otherwise, replace 'tarot-card' with the appropriate ID
+       
         const card = document.getElementById('tarot-card-btn') || document.querySelector('.cardslide');
 
         if (card) {
@@ -57,13 +51,12 @@ const TarotReading = () => {
     function readingRequested() {
         setData(null);
         setIsReadingRequested(true);
-        //setIsCardClicked(false);
         setAnimateBackground(true);
-        //setData(null);
+        
         setTimeout(() => {
             setAnimateBackground(false);
             setCardsDisplayed(true);
-            //getData();
+            
         }, "2500");
         
     }
@@ -72,16 +65,10 @@ const TarotReading = () => {
         
             fetch(`api/${numOfCards}`)
                 .then((results) => {
-                    
                     return results.json();
-                    
                 })
                 .then(res => {
-                    
                     setData(JSON.parse(JSON.stringify(res)));
-                    
-                    localStorage.setItem("reading", JSON.stringify(res));
-                    console.log(JSON.stringify(res));
                    
                 });
 
@@ -91,7 +78,7 @@ const TarotReading = () => {
     return (
         <div>
             {starryBackground} 
-            {/*isReadingOnDisplay && <TarotSelector/>*/}
+            
             <div className="container">
             < motion.div
                 
@@ -176,136 +163,3 @@ const TarotReading = () => {
 };
 export default TarotReading;
 
-{ /*import React, { useState, useRef } from 'react';
-import TarotCard from './TarotCard';
-import '../custom.css';
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { motion } from "framer-motion";
-import './TarotReading.css'; 
-//import SpreadCardStack from './SpreadCardStack';
-//import CardFlip from './CardFlip';
-import { Button, Row, Col } from 'reactstrap';
-
-import StarryBackground from './StarryBackground';
-import CardsSlide from './CardsSlide';
-
-const TarotReading = () => {
-    const [data, setData] = useState(null);
-    const [isReadingOnDisplay, setIsReadingOnDisplay] = useState(false);
-    const [numOfCards, setNumOfCards] = useState(1);
-    const [animateBackground, setAnimateBackground] = useState(false);
-    const scrollRef = useRef(null);
-    const [isCardClicked, setIsCardClicked] = useState(false);
-
-    const updateIsCardClicked = (value) => {
-        setIsCardClicked(value);
-        //console.log("clicked!");
-        let card = document.getElementById('tarot-card');
-    };
-    //const styleAnimation = ` ${isCardClicked ? 'display-reading' : 'dont-display'}`;
-    //const styleAnimation = `'dont-display'`;
-    const pickedCards = (num) => {
-        setNumOfCards(num);
-        
-    };
-    let starryBackground = (<StarryBackground
-            densityRatio={0.75} // Set default values or customize as needed
-            sizeLimit={10}
-            scaleLimit={15}
-            proximityRatio={0.2}
-            isAnimationActive={animateBackground}
-            className="backgroundSetting"
-        />
-   );
-    function readingRequested() {
-        setIsCardClicked(false);
-        setAnimateBackground(true);
-        //setData(null);
-        setTimeout(() => {
-            setAnimateBackground(false);
-            getData();
-        }, "2500");
-        
-    }
-
-    function getData() {
-        
-            fetch(`api/${numOfCards}`)
-                .then((results) => {
-                    
-                    return results.json();
-                    
-                })
-                .then(res => {
-                    
-                    setData(JSON.parse(JSON.stringify(res)));
-                    
-                    localStorage.setItem("reading", JSON.stringify(res));
-                    console.log(JSON.stringify(res));
-                   
-                });
-
-    }
-
-   
-    return (
-        <div>
-            {starryBackground} 
-            
-            <div className="container">
-            < motion.div
-                
-                initial={{ opacity: 0.5, y: 400 }
-                }
-                animate={{
-                    opacity: 1, y:100}}
-                transition={{
-                    duration: 0.7,
-                    delay: 0.8,
-                    ease: [0, 0.71, 0.2, 1.01]
-                }}
-            ><Row>
-
-
-                        <Col className="d-flex justify-content-center flex-column align-items-center" > <h1 >Simple Tarot App</h1> </Col>
-
-                </Row>
-                
-                </motion.div>
-
-           </div>
-            < motion.div
-                className="box"
-                initial={{ opacity:0, y: 500 }
-                }
-                animate={{ y: 250, opacity: 1 }}
-                transition={{
-                    duration: 0.7,
-                    delay: 1.5,
-                    ease: [0, 0.71, 0.2, 1.01]
-                }}
-            ><Row className="justify-content-center">
-                    <Col xs="12" className="d-flex justify-content-center">
-                        {isReadingOnDisplay ? (<div>
-                            <button className="btn" onClick={() => { setIsReadingOnDisplay(!isReadingOnDisplay); }}> Back
-                            </button>
-                        </div>) : (<div>
-                            <button className="btn" onClick={() => { setData(null); console.log(data); readingRequested(); 
-                                setIsReadingOnDisplay(!isReadingOnDisplay); }}>
-                       Get my reading
-                        </button> </div>)}
-                    </Col>
-                </Row></motion.div>
-            <Row className="justify-content-center">
-                <Col xs="12" className="d-flex justify-content-center">{data && isReadingOnDisplay ? (<div className="justify-content-md-center">
-                    <CardsSlide clickCard={updateIsCardClicked} /> <TarotCard id="tarot-card" style={{ display: "none" }} cards={data} /> </div>) : (<div></div>)}
-                
-         </Col>
-                </Row>  
-    </div >
-
-      );
-
-};
-export default TarotReading;
-*/}
